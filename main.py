7a9bafbd9585d4ed3368fc23cd189565a13ecf32
Eddy Javier Jorge Herrera <eddyjj92@gmail.com>
2025-07-03 12:36:47 -0400

    Cargando mensaje aleatorio en primera interaccion v3

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if DEVELOPMENT == 'True':
 model = ChatOpenAI(
     api_key=OPENAI_API_KEY,
     model=OPENAI_API_MODEL,
-    temperature=0.4,
+    temperature=0.6,
     top_p=0.85,
     openai_proxy=OPENAI_PROXY
 )
